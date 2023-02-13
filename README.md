ff725a9e93a6defa7e319d81c8ea991fda140916
Vova <vkhdk@yandex.ru>
2023-02-13 21:24:07 +0300

    update and check pgadmin

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # nifi_docker
+
+# pgadmin
+run on host http://localhost:5050/
+use password from DB to connect
+after login you should add new DB
---
+++ docker/file_share/jupyter_data/python_scripts/ETL_secrets.py
@@ -0,0 +1,11 @@
+#here are all the links and authorization data
+
+# API credentials
+
+#DWH credentials from docker-compose.yml
+postgres_db = 'dwh_postgres_db'
+#hostname from docker-compose.yml
+postgres_host = 'dwh_postgres_container'
+postgres_user = 'dwh_postgres'
+postgres_password = 'dwh_postgres'
+postgres_port = '5432'
